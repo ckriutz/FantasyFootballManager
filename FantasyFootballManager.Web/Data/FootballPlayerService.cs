1da0f233f2dfe0de2b0687327f0f40a785a58813
ckriutz <casey.kriutzfield@live.com>
2022-08-10 11:39:44 -0400

    Change for deplloying to ACI using action.

--- FantasyFootballManager.Web/Data/FootballPlayerService.cs
+++ FantasyFootballManager.Web/Data/FootballPlayerService.cs
@@ -7,17 +7,19 @@ public class FootballPlayerService
     // I basically took this right from: https://docs.microsoft.com/en-us/aspnet/core/blazor/call-web-api?view=aspnetcore-6.0&pivots=server
     HttpClient _client;
     string _baseUrl;
+    string _code;
 
     public FootballPlayerService(IHttpClientFactory clientFactory)
     {
         _client = clientFactory.CreateClient();
         _baseUrl = System.Environment.GetEnvironmentVariable("apiurl");
+        _code = System.Environment.GetEnvironmentVariable("code");
     }
 
     public async Task<List<FootballPlayer>> GetAllFootballPlayersAsync()
     {
         var players = new List<FootballPlayer>();
-        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/players/");
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/players/?code={_code}&clientId=default");
         var response = await _client.SendAsync(request);
 
         if (response.IsSuccessStatusCode)
@@ -32,7 +34,7 @@ public class FootballPlayerService
     public async Task<List<string>> GetAllPositionsAsync()
     {
         List<string> positions = new List<string>();
-        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/positions/");
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/positions/?code={_code}&clientId=default");
 
         var response = await _client.SendAsync(request);
         if (response.IsSuccessStatusCode)
@@ -51,7 +53,7 @@ public class FootballPlayerService
     public async Task<Data.FootballPlayer> GetFootballPlayerAsync(int id)
     {
         var player = new Data.FootballPlayer();
-        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/player/{id}");
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/player/{id}?code={_code}&clientId=default");
 
         var response = await _client.SendAsync(request);
         if (response.IsSuccessStatusCode)
@@ -65,7 +67,7 @@ public class FootballPlayerService
 
     public async Task<bool> UpdatePlayer(Data.FootballPlayer player)
     {
-        var playerResponse = await _client.PutAsJsonAsync<Data.FootballPlayer>($"{_baseUrl}/api/player/", player);
+        var playerResponse = await _client.PutAsJsonAsync<Data.FootballPlayer>($"{_baseUrl}/api/player/?code={_code}&clientId=default", player);
         return playerResponse.IsSuccessStatusCode;
     }
 
