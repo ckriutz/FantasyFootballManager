120a66ad005db834870208bc4314180ab18dc69d
Casey Kriutzfield <casey.kriutzfield@live.com>
2024-09-01 16:35:03 +0000

    Quick Change

--- web/src/Components/AssignPlayerButton.js
+++ web/src/Components/AssignPlayerButton.js
@@ -2,7 +2,7 @@ export default function AssignPlayerButton(props)
 {
     if(props.isOnMyTeam === false && props.isTaken === false)
     {
-        return (<a className="btn btn-primary" onClick={props.assignPlayer}>Assign</a>);
+        return (<a className="btn btn-primary" onClick={props.assignPlayer}>Assigned</a>);
     }
     
 }
